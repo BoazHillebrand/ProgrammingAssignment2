14bd0903179526809f6871a947e6fb8dfa1b3317
Boaz Hillebrand <boazhillebrand@gmail.com>
2016-07-20 17:26:47 -0700

    Solution to cachematrix.R homework.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates the inverse (solution) of a matrix, for later retrieval. 
+## cacheSolve retrieves the inverse if available, or creates a new inverse.
 
-## Write a short comment describing this function
+## Cache the inverse solution of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mat.invert <- NULL
+        set <- function(y){
+                x <<- y
+                mat.invert <<- NULL
+        }
+        get <- function()x
+        set.inverse <- function(inverse)mat.invert <<- inverse
+        get.inverse <- function()mat.invert
+        list(set = set, get = get, set.inverse = set.inverse, get.inverse = get.inverse)
 }
 
 
-## Write a short comment describing this function
+## Get the cached solution for a matrix if it exists, or create it otherwise.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mat.invert <- x$get.inverse()
+        if(!is.null(mat.invert)){
+                return(mat.invert)
+        }
+        data <- x$get()
+        mat.invert <- solve(data,...)
+        x$set.inverse(mat.invert)
+        mat.invert
 }
